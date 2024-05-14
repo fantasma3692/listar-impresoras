40151e467b324f48384ea8cee4bc4a72b74aadb5
fantasma3692 <167818987+fantasma3692@users.noreply.github.com>
2024-05-14 11:22:37 -0500

    Update impresoraRoutes.js

--- impresoraRoutes.js
+++ impresoraRoutes.js
@@ -1,18 +1,21 @@
 // impresoraRoutes.js
 const express = require('express');
+const path = require('path'); // Importa el módulo 'path' de Node.js
 const { execSync } = require('child_process');
 
 const router = express.Router();
 
 router.get('/api/impresora', (req, res) => {
   try {
-    const output = execSync('PrinterRecognitionPlugin.exe').toString();
+    const pluginPath = path.join(__dirname, 'PrinterRecognitionPlugin.exe'); // Construye la ruta completa del archivo
+    const output = execSync(pluginPath).toString();
     const impresoras = output.split('\n').filter(line => line.trim() !== ''); // Parsea la salida del programa C#
     res.json(impresoras);
   } catch (error) {
     console.error('Error al obtener la lista de impresoras:', error);
-    res.status(500).json({ error: 'Error al obtener la lista de impresoras'+ error });
+    res.status(500).json({ error: 'Error al obtener la lista de impresoras: ' + error });
   }
 });
 
 module.exports = router;
+
