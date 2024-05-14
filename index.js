8e6dea2d44d5a1f43f3bf98d8b1b566d9958867d
fantasma3692 <167818987+fantasma3692@users.noreply.github.com>
2024-05-14 13:32:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // server.js
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000; // o el puerto que desees utilizar
 const cors = require('cors');
 const impresoraRoutes = require('./impresoraRoutes');
 app.use(cors());
+app.use(bodyParser.json());
 app.use(impresoraRoutes);
 app.listen(port, () => {
   console.log(`Servidor Node.js escuchando en http://localhost:${port}`);
