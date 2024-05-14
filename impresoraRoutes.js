f030807fea90fdd315d7e5ba8a6bd2f5db43f348
fantasma3692 <167818987+fantasma3692@users.noreply.github.com>
2024-05-14 13:25:28 -0500

    Update impresoraRoutes.js

--- impresoraRoutes.js
+++ impresoraRoutes.js
@@ -1,25 +1,7 @@
 // impresoraRoutes.js
 const express = require('express');
-const { execSync } = require('child_process');
-const printer = require('printer');
 const router = express.Router();
-const directorioEjecucion = '/tmp';
-router.get('/api/impresora', (req, res) => {
-  try {
-    // Mover el archivo a un directorio donde tengamos permisos de ejecución
-    const pluginPath = path.join(__dirname, 'PrinterRecognitionPlugin.exe');
-    const destino = path.join(directorioEjecucion, 'PrinterRecognitionPlugin.exe');
-    fs.renameSync(pluginPath, destino);
-
-    // Ejecutar el archivo desde su nueva ubicación
-    const output = execSync(destino).toString();
-    const impresoras = output.split('\n').filter(line => line.trim() !== ''); // Parsea la salida del programa C#
-    res.json(impresoras);
-  } catch (error) {
-    console.error('Error al obtener la lista de impresoras:', error);
-    res.status(500).json({ error: 'Error al obtener la lista de impresoras: ' + error });
-  }
-});
+const printer = require('printer');
 router.post('/print', (req, res) => {
   const { printerName, data } = req.body;
 
@@ -45,5 +27,5 @@ router.post('/print', (req, res) => {
     console.error('Error en la impresión:', err);
     res.status(500).json({ error: 'Error en la impresión' });
   }
-})
+});
 module.exports = router;
