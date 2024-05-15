4270b9c428e3a742432d8befe7b5413da3c7225f
fantasma3692 <167818987+fantasma3692@users.noreply.github.com>
2024-05-15 07:45:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
-const port = process.env.PORT || 10000; // Usar el puerto definido por la variable de entorno PORT o 10000 como valor predeterminado
+const port = process.env.PORT || 3000; // Usar el puerto definido por la variable de entorno PORT o 10000 como valor predeterminado
 app.use(cors()); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -55,6 +55,6 @@ app.post('/print', async (req, res) => {
   }
 });
 
-app.listen(port,'0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Servidor de impresión escuchando en http://localhost:${port}`);
 });
